f27ba0feed8b6c14ccc54bc82a35b15069f93f47
Jack Maddigan <jack@MacBookPro.localdomain>
2025-04-27 22:03:24 +1200

    update vite config to use es2022, revert supabase_client.js

--- src/lib/supabase_client.js
+++ src/lib/supabase_client.js
@@ -1,22 +1,11 @@
+
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
 
-async function signIn() {
-  try {
-    const { data, error } = await supabase.auth.signInAnonymously({
-      options: {
+const { data, error } = await supabase.auth.signInAnonymously({
+    options: {
         // no options data required for now
-      }
-    });
-
-    if (error) {
-      console.error('Error signing in:', error.message);
     }
-  } catch (err) {
-    console.error('Unexpected error during sign-in:', err);
-  }
-}
-
-signIn();
+  })
