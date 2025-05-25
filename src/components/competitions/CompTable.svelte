a8a90038789eeb798ba173eaea24410559216009
Jack Maddigan <jack@MacBookPro.localdomain>
2025-05-25 23:19:38 +1200

    added comp table colours for diff reg status

--- src/components/competitions/CompTable.svelte
+++ src/components/competitions/CompTable.svelte
@@ -1,7 +1,12 @@
 <script>
-  let width = $state(window.innerWidth);
-
+  import { eventOrder, dateToShort, getRegDate } from "$lib/comp_table_helpers";
   let { type, comps } = $props();
+  for(let i=0; i<comps.length; i++){
+    comps[i].regText = type === "upcoming" ? getRegDate(comps[i]) : "recent" ? "Competition has finished" : null;
+    comps[i].regClosed = comps[i].regText === "Registration is closed";
+  }
+ 
+  let width = $state(window.innerWidth);
 
   const updateWidth = () => {
     width = window.innerWidth;
@@ -12,16 +17,203 @@
 
   // Clean up when component is destroyed
   import { onDestroy } from 'svelte';
-  import DesktopCompTable from './DesktopCompTable.svelte';
-  import MobileCompTable from './MobileCompTable.svelte';
   onDestroy(() => {
     window.removeEventListener('resize', updateWidth);
   });
 </script>
 
-<!-- Render either the desktop version or mobile version  -->
-{#if width < 768}
-  <MobileCompTable {comps} {type}></MobileCompTable>
-{:else}
-  <DesktopCompTable {comps} {type}></DesktopCompTable>
-{/if}
+<table>
+  <tbody>
+    {#each comps as comp}
+      <tr>
+        <td 
+          class:red={type==="recent"} 
+          class:orange={comp.regClosed} 
+          class:green={!comp.regClosed && type==="upcoming"}
+          class:blue={type==="current"}
+        >
+          <!-- Comp name and events -->
+          <div class="col1">
+            <!-- Comp name -->
+            <a 
+              href="https://www.worldcubeassociation.org/competitions/{comp.id}"
+              target="blank"
+              class="comp_name"
+            >
+              {comp.name}
+            </a>
+            <!-- Event icons if desktop only (else they go in row below)-->
+            {#if (width > 768)}
+              <span class="event_icons_row">
+                <p>
+                  {#each comp.events.sort((a, b) => eventOrder.indexOf(a) - eventOrder.indexOf(b)) as event}
+                    <span class="cubing-icon event-{event}"></span>
+                  {/each}
+                </p>
+              </span>
+            {/if}
+          </div>
+          <!-- End comp name and events -->
+
+          <!-- City and venue -->
+          <div class="col2">
+            <span class="city_name">{comp.city}</span>
+            <a
+              href="https://www.google.com/maps?q={comp.venue.coordinates
+                .latitude},{comp.venue.coordinates.longitude}"
+              target="_blank"
+              class="venue_name"
+            >
+              <i>{comp.venue.name}</i>
+            </a>
+          </div>
+          <!-- End city and venue -->
+
+          <!-- Date and reg -->
+          <div class="col3">
+            <span class="comp_date">
+              {dateToShort(comp.date.from)}{comp.date.from === comp.date.till
+              ? ""
+              : `-${dateToShort(comp.date.till)}`}
+            </span>
+            {#if type !== "current"}
+              <a href={comp.externalWebsite || `https://www.worldcubeassociation.org/competitions/${comp.id}/register`}
+                target="_blank" class="reg_date">
+                <i>{comp.regText}</i>
+              </a>
+            {/if}
+          </div>
+          <!-- End date and reg -->
+
+          <!-- Events list for mobile -->
+          {#if (width < 768)}
+            <div class="col4">
+              <span class="event_icons_row">
+                <p>
+                  {#each comp.events.sort((a, b) => eventOrder.indexOf(a) - eventOrder.indexOf(b)) as event}
+                    <span class="cubing-icon event-{event}"></span>
+                  {/each}
+                </p>
+              </span>
+            </div>
+          {/if}
+          <!-- End events list for mobile -->
+        </td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<style>
+  table{
+    width: 100%;
+    table-layout: fixed;
+    max-width: 900px;
+    border-collapse: collapse;
+  }
+  td{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  tr{
+    border: 1px solid var(--colorBlack1);
+  }
+  tbody tr {
+    background-color: var(--colorCompTable1);
+  }
+  tbody tr:nth-child(even) {
+    background-color: var(--colorCompTable2);
+  }
+  .col1, .col2, .col3, .col4 {
+    display: flex;
+    flex-direction: column;
+    padding: 4px 8px;
+    box-sizing: border-box;
+    justify-content: center;
+  }
+  .col1 {
+    width: 40%;
+  }
+  .col2, .col3 {
+    width: 30%;
+  }
+  .col4{
+    width: 100%;
+  }
+  .comp_name, .city_name, .comp_date{
+    font-size: clamp(1rem, 1.2vw, 1.3rem);
+    color: white;
+  }
+  .venue_name, .reg_date, .cubing-icon {
+    font-size: clamp(0.7rem, 0.9vw, 0.9rem);
+  }
+  .venue_name, .cubing-icon{
+    color: var(--colorGrey1);
+  }
+
+  /* RED */
+  .red .reg_date{
+    color: var(--colorRed1);
+  }
+  .red{
+    border-left: 2px solid var(--colorRed1);
+  }
+
+  /* ORANGE */
+  .orange .reg_date{
+    color: orange;
+  }
+  .orange{
+    border-left: 2px solid orange;
+  }
+
+  /* GREEN */
+  .green .reg_date{
+    color: var(--colorGreen1);
+  }
+  .green{
+    border-left: 2px solid  var(--colorGreen1);
+  }
+
+  /* BLUE */
+  .blue{
+    border-left: 2px solid  var(--colorBlue2);
+  }
+
+  .cubing-icon{
+    padding: 2px;
+  }
+  /* .upcoming td{
+    border-left: 4px solid var(--colorGreen1);
+  }
+  .recent td{
+    border-left: 4px solid var(--colorRed1);
+  }
+  .current td{
+    border-left: 4px solid var(--colorBlue2);
+  } */
+
+  @media screen and (max-width: 768px){
+    .comp_name, .comp_date{
+      font-size: clamp(1rem, 1.2vw, 1.3rem);
+      font-weight: 500;
+      color: white;
+    }
+
+    .city_name{
+      font-size: clamp(0.8rem, 1.05vw, 1.1rem);
+      color: white;
+    }
+
+    .venue_name, .reg_date, .cubing-icon {
+      font-size: clamp(0.7rem, 0.9vw, 0.9rem);
+    }
+    .venue_name, .cubing-icon{
+      color: var(--colorGrey1);
+    }
+    .reg_date{
+      color: var(--colorGreen1);
+    }
+  }
+</style>
