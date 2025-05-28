8963d7efb39b878edcba075cb77707f36350f476
Jack Maddigan <jack@MacBookPro.localdomain>
2025-05-28 12:12:53 +1200

    update link

--- src/components/about-us/SiteInfo.svelte
+++ src/components/about-us/SiteInfo.svelte
@@ -4,7 +4,7 @@
   <a href="https://www.instagram.com/corysonntag.photography/">Cory Sontag</a>,
   <a href="https://www.instagram.com/lint.___/?hl=en">Lint</a>,
   <a href="https://www.instagram.com/cyansandwich/">Tom Nelson</a>, and
-  <a href="https://www.instagram.com/noelle.mak2105/">Noelle Mak</a>. 
+  <a href="https://www.instagram.com/nmak.jpg">Noelle Mak</a>. 
   Website design by Jack Maddigan and James Macdiarmid.
 </p>
 
