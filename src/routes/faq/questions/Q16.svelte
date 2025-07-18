0e9a34b01385f30d0a3d89551664e915bb17f0d0
Oscar Welton-Rosie <185318677+OWelton-Rosie@users.noreply.github.com>
2025-07-19 10:05:53 +1200

    make external links open in a new tab

--- src/routes/faq/questions/Q16.svelte
+++ src/routes/faq/questions/Q16.svelte
@@ -1,7 +1,7 @@
 <p>
   Of course! Competitions are completely volunteer-run, so we welcome any help. A great way to get 
   started is by judging — read the official 
-  <a href="https://documents.worldcubeassociation.org/edudoc/judge-tutorial/judge-tutorial.pdf">
+  <a href="https://documents.worldcubeassociation.org/edudoc/judge-tutorial/judge-tutorial.pdf" target="_blank">WCA
   Judging Tutorial</a> to learn how, and ask for an explanation on the day if you want any 
   additional guidance.
 </p>
