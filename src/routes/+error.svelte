02bf4984b8729d7547e27821e5b46bcdf1b8c865
Oscar Welton-Rosie <oweltonrosie@gmail.com>
2026-01-06 17:09:16 +1300

    add WIP +error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let error;
+	export let status;
+
+   import Header from '$lib/components/Header.svelte';
+   import Footer from '$lib/components/Footer.svelte';
+</script>
+
+
+<div class="error-text">
+<h1>{status}</h1>
+
+<h2>
+    {#if status === 404}
+        Page not found.
+    {:else}
+        {error?.message ?? 'Something went wrong.'}
+    {/if}
+</h2>
+
+<a href="/">Go home</a>
+</div>
+
+
+<style>
+    .error-text {
+        text-align: center;
+        margin-bottom: 10%
+    }
+</style>
