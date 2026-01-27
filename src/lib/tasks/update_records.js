e36d5ed229893cc210e59f794ca7c913b4c1eca7
Jack Maddigan <jack.g.maddigan@gmail.com>
2026-01-27 21:09:18 +1300

    Use new records source

--- src/lib/tasks/update_records.js
+++ src/lib/tasks/update_records.js
@@ -1,93 +1,140 @@
-import fs from "fs";
+import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import { centiToDisplay, decodeMbldResult } from "../helpers.js";
+import { centiToDisplay, decodeMbldResult } from '../helpers.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, '../data/records.json');
 
-async function run(){
-  const events = ["333", "222", "444", "555", "666", "777", "333bf", "333fm", "333oh", 
-    "clock", "minx", "pyram", "skewb", "sq1", "444bf", "555bf", "333mbf"]
+const events = [
+  '333',
+  '222',
+  '444',
+  '555',
+  '666',
+  '777',
+  '333bf',
+  '333fm',
+  '333oh',
+  'clock',
+  'minx',
+  'pyram',
+  'skewb',
+  'sq1',
+  '444bf',
+  '555bf',
+  '333mbf',
+];
 
-  const contents = fs.readFileSync(filePath);
-  const data = contents.length ? JSON.parse(contents) : {};
-  
-  // make object of eventId => array of people
-  for(const eventId of events){
-    const nowSingle = await getRecord(eventId, "single");
-    const nowAverage = eventId === "333mbf" ? null : await getRecord(eventId, "average");
-    
-    const wasSingle = data?.[eventId]?.single;
-    const wasAverage = data?.[eventId]?.average;
-
-    data[eventId] = {
-      "single": updateRecord(nowSingle, wasSingle, "single"),
-      "average": updateRecord(nowAverage, wasAverage, "average")
-    }
+async function fetchRecords() {
+  const response = await fetch(
+    `https://raw.githubusercontent.com/speedcubingnz2025/ResultsExport/main/output/national_records.json`,
+  );
+  if (!response.ok) {
+    throw new Error('Bad response!');
   }
-
-  fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-  console.info("DONE UPDATING RECORDS")
+  return await response.json();
 }
 
-/**
- * return updated record array
- */
-function updateRecord(updated, record, type){
-  // if there was to be a new event, then it would not exist, so return the new records
-  if(!record || record.length == 0){ return updated; }
+function getRecord() {}
 
-  // if the result record improved, we don't need to keep any of the old ones, so return the new records
-  const resultImproved = updated[0].best != record[0].best;
-  if(resultImproved){ return updated; }
+async function run() {
+  const newData = await fetchRecords();
 
-  // if the result is the same, we need to check to see if someone new tied the record
-  const recordStrings = record.map(recordToString);
+  const fileContent = fs.readFileSync(filePath);
+  const oldData = fileContent.length ? JSON.parse(fileContent) : {};
 
-  // add item from updated to record if it was not in there already
-  for(const updatedItem of updated){
-    if(!recordStrings.includes(recordToString(updatedItem))){ record.push(updatedItem); }
+  const result = events.reduce((acc, eventId) => {
+    const single = newData.singles
+      .filter((s) => s.event_id === eventId)
+      .map((s) => {
+        const bestText = displayEventResult(s.best, eventId);
+        return {
+          personId: s.person_id,
+          eventId: s.event_id,
+          best: bestText,
+          name: s.person_name,
+          link:
+            oldData[eventId]?.single.find(
+              (c) => c.personId === s.person_id && c.best === bestText,
+            )?.link || '',
+        };
+      });
+    const average = newData.averages
+      .filter((a) => a.event_id === eventId)
+      .map((a) => {
+        const avgText = displayEventResult(a.average, eventId);
+        return {
+          personId: a.person_id,
+          eventId: a.event_id,
+          best: avgText,
+          name: a.person_name,
+          link:
+            oldData[eventId]?.average.find(
+              (c) => c.personId === a.person_id && c.best === avgText,
+            )?.link || '',
+        };
+      });
+    acc[eventId] = {
+      single,
+      average,
+    };
+    return acc;
+  }, {});
+
+  fs.writeFileSync(filePath, JSON.stringify(result, null, 2));
+  console.info('DONE UPDATING RECORDS');
+}
+
+const displayTime = (centi, includeCs = true) => {
+  if (centi === -1) {
+    return 'DNF';
   }
-  
-  return record;
-}
-
-function recordToString(e){
-  return `${e.personId}${e.best}`;
-}
-
-async function getRecord(eventId, type){
-  console.info(`Fetching ${eventId} ${type}<end>`);
-  const response = await fetch(`https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/rank/NZ/${type}/${eventId}.json`);
-  if(!response.ok){ throw new Error("Bad response!"); }
-  let data = (await response.json()).items;
-
-  // initialise with the 1st place result
-  let best = [data.shift()];
-
-  // handle ties by adding while it is equal to prev result
-  while(data[0] && data[0].best === best[0].best){ best.push(data.shift()); }
-
-  // map raw values to display values
-  best.forEach(b =>  b.best = eventId === "333mbf" ? decodeMbldResult(b.best) : eventId==="333fm" && type==="single" ? `${b.best}` :  centiToDisplay(b.best));
-
-  // add the person name since each result will only have WCA id so have to spam some fetches again
-  for(const entry of best){ 
-    entry.name = await getName(entry.personId); 
-    entry.link = "";
-    delete entry.rank; 
-    delete entry.rankType; 
+  if (centi === -2) {
+    return 'DNS';
   }
+  const hours = Math.floor(centi / 36e4);
+  const minutes = Math.floor((centi % 36e4) / 6e3);
+  const seconds = Math.floor((centi % 6e3) / 100);
+  const centiseconds = centi % 100;
+  const hText = hours > 0 ? `${hours}:` : '';
+  const mText =
+    hours > 0
+      ? `${minutes.toString().padStart(2, '0')}:`
+      : minutes > 0
+        ? `${minutes}:`
+        : ``;
+  const sText =
+    minutes > 0 || hours > 0
+      ? `${seconds.toString().padStart(2, '0')}`
+      : `${seconds}`;
+  const cText = includeCs ? `.${centiseconds.toString().padStart(2, '0')}` : '';
+  return `${hText}${mText}${sText}${cText}`;
+};
 
-  return best;
-}
+const displayMulti = (value) => {
+  if(value === -1) return 'DNF';
+  if(value === -2) return 'DNS';
+  const MM = value % 100;
+  const TTTTT = Math.floor(value / 100) % 1e5;
+  const difference = 99 - Math.floor(value / 1e7);
+  const time = displayTime(TTTTT * 100, false);
+  const solved = difference + MM;
+  const attempted = solved + MM;
+  return `${solved}/${attempted} ${time}`;
+};
 
-async function getName(wcaId){
-  console.info(`Fetching ${wcaId}`);
-  const response = await fetch(`https://raw.githubusercontent.com/robiningelbrecht/wca-rest-api/master/api/persons/${wcaId}.json`);
-  if(!response.ok){ throw new Error("Bad response!"); }
-  return (await response.json()).name;
-}
+const displayEventResult = (value, event) => {
+  if (event === '333fm') {
+    // bad code to avoid annoying stuff
+    if(value < 0) return value === -1 ? 'DNF' : 'DNS';
+    if(value < 99) return value;
+    return value / 100;
+  }
+  if (event === '333mbf') {
+    return displayMulti(value);
+  }
+  return displayTime(value);
+};
 
 run();
